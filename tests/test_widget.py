855f1760ceb1a04e70acbc5fe0e31ce5ed40aa6b
Helen Chernotalova <chernotalova.e@yandex.ru>
2024-06-17 22:33:44 +0400

    'Добавление теста на sort_by_data'

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -11,8 +11,12 @@ from src.widget import get_data, mask_account_card
     ],
 )
 def test_mask_account_card(my_facts, expected):
+    """Тест маскировки номера счета или карты с названием"""
+
     assert mask_account_card(my_facts) == expected
 
 
 def test_get_data(string_error):
+    """Тест сортировки даты в функции sort_data"""
+
     assert get_data([]) == string_error
