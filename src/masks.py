35d0f3fb76b832d76af5bdc36d16fdaac73902c4
Helen Chernotalova <chernotalova.e@yandex.ru>
2024-08-05 16:11:33 +0400

    'ДЗ 12_2, работа с логированием + актуализация файла README'

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,22 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(f"../logs/src.masks.log", mode="w")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Преобразует номер карты в маску карты"""
 
+    logger.info("Маскируем номер карты")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Преобразует номер счета в маску счета"""
 
+    logger.info("Маскируем номер счета")
     return f"**{account_number[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,34 @@
 import json
+import logging
 import os
 from typing import Dict, List
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(f"../logs/src.utils.log", mode="w")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 
 def get_transactions(path: str) -> List[Dict]:
     """Возвращает список словарей, содержащих данные об транзакциях"""
 
     if not os.path.exists(path):
+        logger.error("Файл пустой")
         return []
 
     try:
+        logger.info("Получаем данные о транзакциях в формате JSON")
         with open(path, "r", encoding="utf-8") as file:
             transaction = json.load(file)
 
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as er:
+        logger.error(f"Произошла ошибка в файле формата JSON: {er}")
         return []
 
     if not isinstance(transaction, list):
+        logger.error("Некорректный тип данных в файле формата JSON")
         return []
 
     return transaction
