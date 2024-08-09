b609e8bb3d5b4820d66163b7e384df474ba0a161
Helen Chernotalova <chernotalova.e@yandex.ru>
2024-08-10 00:03:06 +0400

    'ДЗ 13_2, создание новых функций и модулей'

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,16 @@
+import re
+from collections import Counter
+
+from src.utils import get_transactions
+
+
 def filter_by_state(operation_list: list, key_state: str = "EXECUTED") -> list:
     """Функция возвращающая список операций с определенным состоянием"""
 
     filtered_list = []
-    for i in range(len(operation_list)):
-        if operation_list[i]["state"] == key_state:
-            filtered_list.append(operation_list[i])
+    for i in operation_list:
+        if i["state"] == key_state:
+            filtered_list.append(i)
     return filtered_list
 
 
@@ -13,3 +19,29 @@ def sort_by_date(infor_operation: list, is_descending: bool = True) -> list:
 
     sorted_list = sorted(infor_operation, key=lambda operation: operation.get("date"), reverse=is_descending)
     return sorted_list
+
+
+def search_by_description(operations: list[dict], user_search: str) -> list[dict]:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска
+    и возвращает список словарей, у которых в описании есть данная строка
+    """
+
+    return [operation for operation in operations if re.search(user_search.lower(), operation["description"].lower())]
+
+
+def get_count_operations_by_category(operations: list[dict], list_of_category: list) -> dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций
+    и возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории
+    """
+    result = Counter(
+        [operation["description"] for operation in operations if operation["description"] in list_of_category]
+    )
+
+    return dict(result)
+
+
+if __name__ == "__main__":
+    file = get_transactions("../data/operations.json")
+    print(file)
+    print(filter_by_state(file))
